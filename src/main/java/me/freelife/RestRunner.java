7990d5c5e72cd9f3e6a6f524368589d4e66435ec
freelife1191 <freelife1191.good@gmail.com>
2019-01-28 01:31:26 +0900

    스프링 REST 클라이언트 1부: WebClient

--- src/main/java/me/freelife/RestRunner.java
+++ src/main/java/me/freelife/RestRunner.java
@@ -3,33 +3,57 @@ package me.freelife;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class RestRunner implements ApplicationRunner {
 
     @Autowired
-    RestTemplateBuilder restTemplateBuilder;
+    WebClient.Builder builder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        RestTemplate restTemplate = restTemplateBuilder.build();
+        WebClient webClient = builder.build();
 
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        // TODO /hello
-        String helloResult = restTemplate.getForObject("http://localhost:8080/hello", String.class);
-        System.out.println(helloResult);
+        // Stream API는 Subscribe 하기 전에는 Stream이 흐르지 않음 그냥 담아만 놓은 것 뿐임
+        // 아무동작을 하지 않고 단지 Mono만 만들어 둠
+        Mono<String> helloMono = webClient.get().uri("http://localhost:8080/hello") // get으로 /hello 요청
+                .retrieve() // 응답값을 가져옴
+                .bodyToMono(String.class);// Mono Type으로 변경
 
-        // TODO /world
-        String worldResult = restTemplate.getForObject("http://localhost:8080/world", String.class);
-        System.out.println(worldResult);
+        // hello subscribe 결과값은 String
+        // 비동기방식 이라 subscribe하고 Callback이 오면 로직을 수행
+        helloMono.subscribe(s -> {
+            System.out.println(s);
 
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint());
+            if(stopWatch.isRunning()) { // stopwatch가 실행중이면 종료
+                stopWatch.stop();
+            }
+
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start();
+        });
+
+
+        Mono<String> worldMono = webClient.get().uri("http://localhost:8080/world")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        worldMono.subscribe(s -> {
+            System.out.println(s);
+
+            if(stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start();
+        });
     }
 }
